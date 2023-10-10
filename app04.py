50a0febabadc1b2ea634e6c8772655757606b193
JungKyu-Kim <jungkyu2.kim@doosan.com>
2023-10-10 04:44:19 +0000

    commit

--- app04.py
+++ app04.py
@@ -1,16 +1,15 @@
 import streamlit as st
 
 init_name = '홍길동'
-init_fact_gathering = """
--산학 협력 과제 3건 달성
+init_fact_gathering = 
+"""-산학 협력 과제 3건 달성
  + 반도체 수요예측 과제
  + 물류 최저화 과제
  + Vision 품질 검사 모델 개발 과제
 -기술 내재화 1건 : Vision 품질 검사 모델 특허 출원
 -서울대학교 교수진 및 사내 과제를 매칭하고 과제를 추진하는 전체 과정을 리드하였음
 -3개 과제를 선정하여 산학협력을 추진하였음
--Vision 품질 검사 모델은 특허 출원하여 사내 AI 역량을 내재화 하였음
-"""
+-Vision 품질 검사 모델은 특허 출원하여 사내 AI 역량을 내재화 하였음"""
 
 def draw_ui():
     # title and description
@@ -48,6 +47,7 @@ def draw_ui():
         'Marginally Meets' : 2,
         'Does not Meet' : 1
     }
+    st.write('타입', type(grade))
     st.write('피드백 등급:', grade[input_selected_grade])
 
     # Temperature
