77c99cf5e025adfabac757e335ece919ccda4bf7
JungKyu-Kim <jungkyu2.kim@doosan.com>
2023-10-10 05:40:36 +0000

    commit

--- app04.py
+++ app04.py
@@ -11,6 +11,27 @@ init_fact_gathering = """
 -3개 과제를 선정하여 산학협력을 추진하였음
 -Vision 품질 검사 모델은 특허 출원하여 사내 AI 역량을 내재화 하였음"""
 
+min_length_of_result = 100
+init_length_of_result = 300
+
+# feedback grade 셋팅값
+grade_set = {
+    'Highly Exceeds' : 5,
+    'Exceeds' : 4,
+    'Meets' : 3,
+    'Marginally Meets' : 2,
+    'Does not Meet' : 1
+}
+grade_list = list(grade_set.keys())  
+
+# Temperature 셋팅값
+temperature_set = {
+    '일관적' : 0.5,
+    '보통' : 1,
+    '창의적' : 1.5
+}
+temperature_list = list(temperature_set.keys())  
+
 def draw_ui():
     # title and description
     st.title("Peoply FeedbackGPT")
@@ -29,43 +50,33 @@ def draw_ui():
         )
 
     # feedback grade
-    grade_set = {
-        'Highly Exceeds' : 5,
-        'Exceeds' : 4,
-        'Meets' : 3,
-        'Marginally Meets' : 2,
-        'Does not Meet' : 1
-    }
-    grade_list = list(grade_set.keys())  
-    input_selected_grade = st.selectbox(
+    input_grade_text = st.selectbox(
         '피드백 등급',
         grade_list,
         index=2
         )
-
-    st.write('피드백 등급:', grade_set[input_selected_grade])
+    input_grade = grade_set[input_grade_text]
 
     # Temperature
-    input_selected_temperature = st.select_slider(
+    input_temperature_text = st.select_slider(
         '피드백 다양성',
-        options=['일관적', '보통', '창의적']
+        options=temperature_list
         )
-    temperature = {
-        '일관적' : 0.5,
-        '보통' : 1,
-        '창의적' : 1.5
-    }
-
-    st.write('피드백 다양성:', temperature[input_selected_temperature])
+    input_temperature = temperature_set[input_temperature_text]
 
     # length of result
-    min_length_of_result = 100
-    init_length = 300
     input_length = st.number_input(
         '글자수',
         min_value=min_length_of_result,
         value=init_length,
         format="%d"
         )
+
+    st.write('---------------')
+    st.write('이름', input_name)
+    st.write('Fact Gathering', input_fact_gathering)
+    st.write('피드백 등급', input_grade)
+    st.write('피드백 다양성', input_temperature)
+    st.write('글자수', input_length)
 
 draw_ui()
