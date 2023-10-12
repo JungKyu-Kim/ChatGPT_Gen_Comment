628cc76dbcfb56703e095dc71ed7d5070bcf767a
JungKyu-Kim <jungkyu2.kim@doosan.com>
2023-10-12 07:44:38 +0000

    commit

--- app04.py
+++ app04.py
@@ -56,50 +56,50 @@ class Input():
 if 'inp' not in st.session_state:
     st.session_state.inp = []
 
-# def add_form():
-#     st.session_state.inp.append(Input(input_name, input_fact_gathering, input_grade, input_temperature, submitted))
+def add_form():
+    st.session_state.inp.append(Input(input_name, input_fact_gathering, input_grade, input_temperature, submitted))
 
-# with st.form(key="my_form"):
-#     # title and description
-#     st.title("Peoply FeedbackGPT")
-#     st.text("Peoply FeedbackGPT 입니다.")
+with st.form(key="my_form"):
+    # title and description
+    st.title("Peoply FeedbackGPT")
+    st.text("Peoply FeedbackGPT 입니다.")
 
-#     # Name
-#     input_name = st.text_input(
-#         '이름',
-#         init_name
-#         )
+    # Name
+    input_name = st.text_input(
+        '이름',
+        init_name
+        )
 
-#     # fact_gathering text area
-#     input_fact_gathering = st.text_area(
-#         "Fact Gathering",
-#         init_fact_gathering
-#         )
+    # fact_gathering text area
+    input_fact_gathering = st.text_area(
+        "Fact Gathering",
+        init_fact_gathering
+        )
 
-#     # feedback grade
-#     input_grade_text = st.selectbox(
-#         '피드백 등급',
-#         grade_list,
-#         index=2
-#         )
-#     input_grade = grade_set[input_grade_text]
+    # feedback grade
+    input_grade_text = st.selectbox(
+        '피드백 등급',
+        grade_list,
+        index=2
+        )
+    input_grade = grade_set[input_grade_text]
 
-#     # Temperature
-#     input_temperature_text = st.select_slider(
-#         '피드백 다양성',
-#         options=temperature_list
-#         )
-#     input_temperature = temperature_set[input_temperature_text]
+    # Temperature
+    input_temperature_text = st.select_slider(
+        '피드백 다양성',
+        options=temperature_list
+        )
+    input_temperature = temperature_set[input_temperature_text]
 
-#     # length of result
-#     input_length = st.number_input(
-#         '글자수',
-#         min_value=min_length_of_result,
-#         value=init_length_of_result,
-#         format="%d"
-#         )
+    # length of result
+    input_length = st.number_input(
+        '글자수',
+        min_value=min_length_of_result,
+        value=init_length_of_result,
+        format="%d"
+        )
 
-#     st.form_submit_button("Submit",on_click=add_form)
+    st.form_submit_button("Submit",on_click=add_form)
 
 # def draw_result(input_name, input_fact_gathering, input_grade, input_temperature, input_length):
 #     st.write('---------------')
